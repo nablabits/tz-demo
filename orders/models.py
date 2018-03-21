6575e87473290bb6e8410575ee85982c7163f70f
nablabits <birrick@gmail.com>
2018-03-21 15:48:21 +0100

    initial commit

---
+++ orders/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Customers(models.Model):
+    """Create the customer db table."""
+    pass
+
+
+class Orders(models.Model):
+    """Order entries enter here."""
+    pass
+
+
+class Comments(models.Model):
+    """A general comment system linked to each order."""
+    pass
