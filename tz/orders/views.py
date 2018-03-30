70359b6ef8b43d9b6623769a8220fb84cd3757a8
nablabits <birrick@gmail.com>
2018-03-30 16:19:01 +0200

    Defining views

--- tz/orders/views.py
+++ tz/orders/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Comment, Customer, Order, CommentCheck
 
-# Create your views here.
+def main(request):
+    """The root view."""
+
+    # Query all active orders
+    orders = Order.objects.exclude(status=7).order_by('-inbox_date')
+
+    # Query last commnents on active orders
+    comments = Comment.objects.filter(reference__in=orders)
+    comments = comments.order_by('-creation')
+
+    dict4view = {'orders': orders,
+                 'comments': comments,
+                 }
+
+    return render(request, 'tz/main.html', dict4view)
