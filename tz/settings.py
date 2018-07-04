558dd4f600394452fdbbb764ac57e19eb81e0cfe
nablabits <birrick@gmail.com>
2018-07-04 19:44:02 +0200

    Deploy settings

--- tz/settings.py
+++ tz/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
