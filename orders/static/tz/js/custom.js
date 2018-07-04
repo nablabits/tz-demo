38fb93bbdc5ddfa681e1498ee5e5281345ed24f0
nablabits <birrick@gmail.com>
2018-07-04 12:02:43 +0200

    Table row link
    Now each row on orders & customers link to the proper customer/order 
    view

--- orders/static/tz/js/custom.js
+++ orders/static/tz/js/custom.js
@@ -1,7 +1,19 @@
-/* global $   */
-$(function () {
-  $('.js-popover').popover({
-    container: 'body',
-    trigger: 'focus'
+/* global $ location  */
+
+$(document).ready(function () {
+  // Popovers activation
+  $(function () {
+    $('.js-popover').popover({
+      container: 'body',
+      trigger: 'focus'
+    })
   })
+
+  // opens a list item on orders/customers view
+  var openItem = function () {
+    var href = $(this).attr('data-href')
+    $(location).attr('href', href)
+  }
+
+  $('.js-view-list-item').click(openItem)
 })
