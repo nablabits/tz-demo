2acfc994a3eecedf3a925344c1f9ddb211254339
nablabits <birrick@gmail.com>
2018-07-17 13:49:43 +0200

    Model Test suite

--- orders/tests/test_models.py
+++ orders/tests/test_models.py
@@ -1,18 +1,59 @@
 from django.test import TestCase
-from orders.models import Customer
+from orders.models import Customer, Order, Document, OrderItem, Comment
+from django.contrib.auth.models import User
+from datetime import date
 
-class CustomerModelTest(TestCase):
+
+class ModelTest(TestCase):
+    """Test the models."""
 
     @classmethod
     def setUpTestData(cls):
+        """Create the necessary items on database at once."""
+        User.objects.create(username='user', is_staff=True, is_superuser=True)
+
         Customer.objects.create(name='Customer Test',
                                 address='This computer',
                                 city='No city',
                                 phone='666666666',
-                                email='cutomer@example.com',
+                                email='customer@example.com',
                                 CIF='5555G',
                                 cp='48100')
 
+        Order.objects.create(user=User.objects.get(pk=1),
+                             customer=Customer.objects.get(pk=1),
+                             ref_name='example',
+                             delivery=date(2018, 2, 1),
+                             waist=10,
+                             chest=20,
+                             hip=30,
+                             lenght=40,
+                             others='Custom notes',
+                             budget=2000,
+                             prepaid=1000,
+                             workshop=120)
+
+        Comment.objects.create(user=User.objects.get(pk=1),
+                               reference=Order.objects.get(pk=1),
+                               comment='This is a comment')
+
     def test_customer_creation(self):
+        """Test the customer creation."""
         customer = Customer.objects.get(pk=1)
         self.assertTrue(isinstance(customer, Customer))
+        self.assertEqual(customer.__str__(), 'Customer Test')
+
+    def test_order_creation(self):
+        """Test the order creation."""
+        order = Order.objects.get(pk=1)
+        self.assertTrue(isinstance(order, Order))
+        self.assertEqual(order.__str__(), '2018-07-17 Customer Test example')
+        self.assertTrue(order.overdue)
+        self.assertEqual(order.pending, -1000)
+
+    def test_comment_creation(self):
+        comment = Comment.objects.get(pk=1)
+        comment_str = ('El 2018-07-17, user comentó en 2018-07-17 Customer' +
+                       ' Test example')
+        self.assertTrue(isinstance(comment, Comment))
+        self.assertEqual(comment.__str__(), comment_str)
--- tz/settings.py
+++ tz/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'orders',
     'widget_tweaks',
+    'coverage',
 ]
 
 MIDDLEWARE = [
