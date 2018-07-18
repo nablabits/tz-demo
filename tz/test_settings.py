c6a059335962010af6f86738f64441466eb8d720
nablabits <birrick@gmail.com>
2018-07-18 09:59:26 +0200

    test settings to avoid migrations

---
+++ tz/test_settings.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+
+MIGRATION_MODULES = {
+    'auth': None,
+    'contenttypes': None,
+    'default': None,
+    'sessions': None,
+    'messages': None,
+    'orders': None,
+    'admin': None,
+}
