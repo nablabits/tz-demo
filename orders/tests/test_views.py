e61736cac8674a70e4d5bd16f8bbec46b05b87cd
nablabits <birrick@gmail.com>
2018-07-18 12:37:52 +0200

    Test not logged in users redirect

--- orders/tests/test_views.py
+++ orders/tests/test_views.py
@@ -14,6 +14,8 @@ class ViewTest(TestCase):
         # Create users
         User.objects.create_user(username='regular', password='test')
         User.objects.create_user(username='another', password='test')
+        regular = User.objects.get(username='regular')
+        another = User.objects.get(username='another')
 
         # Create a customer
         Customer.objects.create(name='Customer Test',
@@ -23,10 +25,11 @@ class ViewTest(TestCase):
                                 email='customer@example.com',
                                 CIF='5555G',
                                 cp='48100')
+        customer1 = Customer.objects.get(name='Customer Test')
 
         # Create an outdated and unpaid order
-        Order.objects.create(user=User.objects.get(pk=1),
-                             customer=Customer.objects.get(pk=1),
+        Order.objects.create(user=regular,
+                             customer=customer1,
                              ref_name='example',
                              delivery=date(2018, 2, 1),
                              waist=10,
@@ -37,10 +40,11 @@ class ViewTest(TestCase):
                              budget=2000,
                              prepaid=1000,
                              workshop=120)
+        order1 = Order.objects.get(ref_name='example')
 
         # Create a delivered and unpaid order
-        Order.objects.create(user=User.objects.get(pk=1),
-                             customer=Customer.objects.get(pk=1),
+        Order.objects.create(user=regular,
+                             customer=customer1,
                              status=7,
                              ref_name='example delivered',
                              delivery=date(2018, 7, 1),
@@ -54,8 +58,8 @@ class ViewTest(TestCase):
                              workshop=120)
 
         # Create a cancelled order
-        Order.objects.create(user=User.objects.get(pk=1),
-                             customer=Customer.objects.get(pk=1),
+        Order.objects.create(user=regular,
+                             customer=customer1,
                              status=8,
                              ref_name='example cancelled',
                              delivery=date(2018, 7, 1),
@@ -69,12 +73,44 @@ class ViewTest(TestCase):
                              workshop=120)
 
         # Create comments
-        Comment.objects.create(user=User.objects.get(pk=1),
-                               reference=Order.objects.get(pk=1),
+        Comment.objects.create(user=regular,
+                               reference=order1,
                                comment='This is a comment')
-        Comment.objects.create(user=User.objects.get(pk=2),
-                               reference=Order.objects.get(pk=1),
+        Comment.objects.create(user=another,
+                               reference=order1,
                                comment='another')
+
+    def test_not_logged_in_redirect(self):
+        """Not logged in users should go to a login page."""
+        # Main view
+        login_url = '/accounts/login/?next=/'
+        resp = self.client.get(reverse('main'))
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, login_url)
+
+        # Order list
+        login_url = '/accounts/login/?next=/orders'
+        resp = self.client.get(reverse('orderlist'))
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, login_url)
+
+        # Order view
+        login_url = '/accounts/login/?next=/order/view/1'
+        resp = self.client.get(reverse('order_view', kwargs={'pk': 1}))
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, login_url)
+
+        # Customer list
+        login_url = '/accounts/login/?next=/customers'
+        resp = self.client.get(reverse('customerlist'))
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, login_url)
+
+        # Customer view
+        login_url = '/accounts/login/?next=/customer_view/1'
+        resp = self.client.get(reverse('customer_view', kwargs={'pk': 1}))
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, login_url)
 
     def test_main_view(self):
         login = self.client.login(username='regular', password='test')
