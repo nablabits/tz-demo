76159fe4fb1232fa3fec071ee5ec337ebb6189a5
nablabits <birrick@gmail.com>
2018-09-06 12:57:55 +0200

    Get rid login test

--- orders/tests/test_ui.py
+++ orders/tests/test_ui.py
@@ -7,39 +7,6 @@ from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from django.contrib.auth.models import User
 
 
-class LoginTest(LiveServerTestCase):
-    """Test the proper login.
-
-    Since in other views login is set on setUpClass (because is required for
-    all of them), test the login here alone.
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """Set Up the tests in a permanent way."""
-        super().setUpClass()
-        profile = FirefoxProfile()
-        cls.selenium = WebDriver(firefox_profile=profile)
-        cls.selenium.implicitly_wait(10)
-        regular = User.objects.create_user(username='regular', password='test')
-        regular.save()
-
-    @classmethod
-    def tearDownClass(cls):
-        """Deactivate test settings."""
-        cls.selenium.quit()
-        super().tearDownClass()
-
-    def test_login(self):
-        """Test login into the app."""
-        self.selenium.get(self.live_server_url + '/')
-        username_input = self.selenium.find_element_by_id("id_username")
-        username_input.send_keys('regular')
-        password_input = self.selenium.find_element_by_id("id_password")
-        password_input.send_keys('test')
-        self.selenium.find_element_by_id("submit").click()
-
-
 class CreationTest(LiveServerTestCase):
     """Test the proper creation of anyone of the models."""
 
