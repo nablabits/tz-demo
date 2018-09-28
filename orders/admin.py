741391988732789e1658193c2a4e750070e905b4
nablabits <birrick@gmail.com>
2018-09-28 08:06:49 +0200

    Add new models to admin page

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,10 @@
+"""Show all the models in the admin page."""
+
 from django.contrib import admin
-from .models import Customer, Comment, Order
+from .models import Customer, Comment, Order, OrderItem, Timing
 
 admin.site.register(Customer)
 admin.site.register(Comment)
 admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Timing)
