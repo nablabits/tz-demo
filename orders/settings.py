11c7472a821bb0878c678a916fe52de2e47596da
nablabits <birrick@gmail.com>
2018-10-28 21:19:09 +0100

    Item model choices

--- orders/settings.py
+++ orders/settings.py
@@ -1,4 +1,27 @@
 """These are the common settings for the app orders."""
 
 # The name for the app
-APP_NAME = "Trapuzarrak app"
+APP_NAME = "Trapu Zarrak app"
+
+# Available types of clothes
+ITEM_TYPE = (
+    ('0', 'No definido'),
+    ('1', 'Falda'),
+    ('2', 'Pantalón'),
+    ('3', 'Camisa'),
+    ('4', 'Pañuelo'),
+    ('5', 'Delantal'),
+    ('6', 'Corpiño'),
+    ('7', 'Chaleco'),
+    ('8', 'Gerriko'),
+    ('9', 'Bata'),
+    ('10', 'Pololo'),
+    ('11', 'Azpikogona'),
+    ('12', 'Traje de niña'),
+)
+
+ITEM_CLASSES = (
+    ('S', 'Standard'),
+    ('M', 'Medium'),
+    ('P', 'Premium')
+)
