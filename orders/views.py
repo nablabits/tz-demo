68dee6b38fbd833cf093fc0ce47ce1a53f14a376
nablabits <birrick@gmail.com>
2018-11-14 11:55:12 +0100

    Get rid of unused import

--- orders/views.py
+++ orders/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
-from django.db.models import Count, Sum, F, Q
+from django.db.models import Count, Sum, F
 from datetime import datetime
 from . import settings
 import markdown2
