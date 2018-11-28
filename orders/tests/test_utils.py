063df442c06d70182587e755fdaef418a470ca84
nablabits <birrick@gmail.com>
2018-11-28 13:21:15 +0100

    Color utility & tests

--- orders/tests/test_utils.py
+++ orders/tests/test_utils.py
@@ -1,7 +1,9 @@
 """Test utilities."""
 
-from orders.utils import TimeLenght
+from orders.utils import TimeLenght, WeekColor
 from django.test import TestCase
+from datetime import date, timedelta
+from orders import settings
 
 
 class TimeLenghtTest(TestCase):
@@ -55,3 +57,49 @@ class TimeLenghtTest(TestCase):
         """Test the proper conversion of form entries into floats."""
         time = TimeLenght('5:30')
         self.assertEqual(time.convert(), 5.5)
+
+
+class WeekColorTest(TestCase):
+    """test the color returned by the function."""
+
+    def test_only_date_obj_are_allowed(self):
+        """The imput should be a date object, otherwise reaise an error."""
+        with self.assertRaises(TypeError):
+            WeekColor('invalid type')
+
+    def test_this_week_returns_proper_color(self):
+        """The color is stored in settings.py."""
+        delivery = date.today()
+        color = WeekColor(delivery).get()
+        self.assertEqual(color, settings.WEEK_COLORS['this'])
+
+    def test_next_week_returns_proper_color(self):
+        """The color is stored in settings.py."""
+        delivery = date.today() + timedelta(days=7)
+        color = WeekColor(delivery).get()
+        self.assertEqual(color, settings.WEEK_COLORS['next'])
+
+    def test_in_two_weeks_returns_proper_color(self):
+        """The color is stored in settings.py."""
+        delivery = date.today() + timedelta(days=14)
+        color = WeekColor(delivery).get()
+        self.assertEqual(color, settings.WEEK_COLORS['in_two'])
+
+    def test_more_than_two_weeks_returns_false(self):
+        """The color is stored in settings.py."""
+        delivery = date.today() + timedelta(days=22)
+        color = WeekColor(delivery).get()
+        self.assertFalse(color)
+
+    def test_delivery_on_next_year_returns_false(self):
+        """On changing year."""
+        next_year = date.today().year + 1
+        delivery = date(next_year, 1, 1)
+        color = WeekColor(delivery).get()
+        self.assertFalse(color)
+
+    def test_before_than_this_week_returns_this_week_color(self):
+        """The color is stored in settings.py."""
+        delivery = date.today() - timedelta(days=7)
+        color = WeekColor(delivery).get()
+        self.assertEqual(color, settings.WEEK_COLORS['this'])
--- orders/utils.py
+++ orders/utils.py
@@ -1,5 +1,7 @@
 """Some utilities to use in the app."""
 import re
+from . import settings
+from datetime import date
 
 
 class TimeLenght(object):
@@ -48,3 +50,36 @@ class TimeLenght(object):
             raise ValueError('No valid direction was given')
 
         return conversion
+
+
+class WeekColor(object):
+    """Generate a color depending the delivery's week."""
+
+    def __init__(self, delivery):
+        """Start the object."""
+        if not isinstance(delivery, date):
+            raise TypeError('The imput should be a datetype')
+        self.date = delivery
+
+    def get(self):
+        """Return the proper color."""
+
+        # first get the delivery and current week
+        delivery = self.date.isocalendar()[1]
+        this_week = date.today().isocalendar()[1]
+
+        # If delivery is on next year avoid the isocalendar reset
+        if self.date.year > date.today().year:
+            delivery = delivery + 52
+        elif self.date.year < date.today().year:
+            delivery = delivery - 53
+
+        # Finally evaluate both numbers and return the proper color
+        if delivery <= this_week:
+            return settings.WEEK_COLORS['this']
+        elif delivery == this_week + 1:
+            return settings.WEEK_COLORS['next']
+        elif delivery == this_week + 2:
+            return settings.WEEK_COLORS['in_two']
+        else:
+            return False
