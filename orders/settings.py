894fc5078faad0a63a6781c8802241f71a29b792
David Reguera <birrick@gmail.com>
2018-12-31 09:13:38 +0100

    Merge branch 'feature/year_change'

--- orders/settings.py
+++ orders/settings.py
@@ -2,7 +2,7 @@
 
 # The name for the app
 APP_NAME = "Trapu Zarrak app"
-VERSION = 'v54'  # next version to be delivered
+VERSION = 'v55'  # next version to be delivered
 
 # Available types of clothes
 ITEM_TYPE = (
