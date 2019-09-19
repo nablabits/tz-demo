cef526a2f5d7488800a13d446bbbc92399a414c0
David Reguera <birrick@gmail.com>
2019-09-19 12:47:40 +0200

    Fix the test errors on running the test suite as a block.
    The commom contex test threw errors when ran along with all the test 
    suite, but worked nice just for the CommonContextOrderDetails class.

--- orders/tests/test_views.py
+++ orders/tests/test_views.py
@@ -245,14 +245,19 @@ class CommonContextOrderDetails(TestCase):
 
     class Request:
         """Create a dummy request to test the method."""
-        user = User.objects.first()
+
+        def __init__(self, user):
+            """Create the needed user."""
+            self.user = user
 
     @classmethod
     def setUpTestData(cls):
         """Create the necessary items on database at once."""
-        # Create a user
-        u = User.objects.create_user(
+        user = User.objects.create_user(
             username='test user', is_staff=True, is_superuser=True,)
+
+        # Instantiante the dummy request to use during the test suite
+        cls.request = cls.Request(user)
 
         # Create a customer
         c = Customer.objects.create(name='Customer Test', phone=0, cp=48100)
@@ -261,12 +266,12 @@ class CommonContextOrderDetails(TestCase):
         i = Item.objects.create(name='test', fabrics=10, price=30)
 
         # Create a timetable so view doesn't throw an error
-        Timetable.objects.create(user=u)
+        Timetable.objects.create(user=user)
 
         # Create some orders with items
         for n in range(5):
             o = Order.objects.create(
-                user=u, customer=c, ref_name='test%s' % n,
+                user=user, customer=c, ref_name='test%s' % n,
                 delivery=date.today(), )
             OrderItem.objects.create(reference=o, element=i, qty=n)
 
@@ -287,50 +292,44 @@ class CommonContextOrderDetails(TestCase):
             CommonContexts.order_details(request='dummy', pk=4e4)
 
     def test_comments(self):
-        request = self.Request()
+        # request = self.re
         order = Order.objects.first()
         Comment.objects.create(
-            user=request.user, reference=order, comment='test')
-        context = CommonContexts.order_details(request, order.pk)
+            user=self.request.user, reference=order, comment='test')
+        context = CommonContexts.order_details(self.request, order.pk)
         self.assertEqual(context['comments'][0].comment, 'test')
 
     def test_items(self):
-        request = self.Request()
         order = Order.objects.first()
-        context = CommonContexts.order_details(request, order.pk)
+        context = CommonContexts.order_details(self.request, order.pk)
         self.assertEqual(context['items'].count(), 1)
 
     def test_user(self):
-        request = self.Request()
         order = Order.objects.first()
-        context = CommonContexts.order_details(request, order.pk)
-        self.assertEqual(context['user'].username, request.user.username)
+        context = CommonContexts.order_details(self.request, order.pk)
+        self.assertEqual(context['user'].username, self.request.user.username)
 
     def test_session(self):
-        request = self.Request()
         order = Order.objects.first()
-        context = CommonContexts.order_details(request, order.pk)
+        context = CommonContexts.order_details(self.request, order.pk)
         self.assertIsInstance(context['session'], Timetable)
-        self.assertEqual(context['session'].user, request.user)
+        self.assertEqual(context['session'].user, self.request.user)
 
     def test_title(self):
-        request = self.Request()
         order = Order.objects.first()
-        context = CommonContexts.order_details(request, order.pk)
+        context = CommonContexts.order_details(self.request, order.pk)
         self.assertEqual(
             context['title'],
             f'Pedido {order.pk}: Customer Test, {order.ref_name}')
 
     def test_item_times_form(self):
-        request = self.Request()
         order = Order.objects.first()
-        context = CommonContexts.order_details(request, order.pk)
+        context = CommonContexts.order_details(self.request, order.pk)
         self.assertIsInstance(context['update_times'], ItemTimesForm)
 
     def test_static_vars(self):
-        request = self.Request()
         order = Order.objects.first()
-        context = CommonContexts.order_details(request, order.pk)
+        context = CommonContexts.order_details(self.request, order.pk)
         self.assertEqual(context['version'], settings.VERSION)
         self.assertEqual(context['btn_title_add'], 'Añadir prendas')
         self.assertEqual(context['js_action_add'], 'order-item-add')
